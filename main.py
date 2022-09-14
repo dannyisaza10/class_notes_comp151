093ba54988d14673b1f6b67ed34be74497ce2c1e
Danny Isaza <dannyisaza@MacBook-Pro-3.local>
2022-09-14 19:54:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+# Let's experiment with variables and data types.
+
+# This is a string
+"Hello World"
+# Lets print it
+print("Hello World")
+
+print('hello world')
+
+# this is a floating point number
+print(17.0)
+
+print(2+17.0)
+
+print(2/3)
+
+print(2//3)
+
+print(17 % 2)
+
+# THe ** is for exponents or powers
+print(3**2)
+
+print(2**3)
+
+# Remember the PEMDAS order of operations
+
+print(14_000_000)
+
+print(2 * 3)
+
+msg = "Hello world"
+
+print(msg)
+
+msg2 = "What is your name?"
+print(msg2)
+
+class_grade = 4.0
+
+class_credits = 3
+
+print(class_credits)
+
+quality_points = class_grade * class_credits
+
+print(quality_points)
+
+fname = "dan"
+
+lname = "isaza"
+
+print(fname)
+
+first_name, last_name = "dan", "isaza"
+
+print(first_name)
+
+print(type(msg))
+
+print(type(class_grade))
+
+print(type(class_credits))
+
+# Use all caps for constants that you never want to change
+NATION_NAME = "USA"
